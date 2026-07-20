fef4bb7b1541461af541d43d81e83f9ffd0ee013
fishstiz <fishstickz7777@gmail.com>
2026-07-20 11:24:14 +0800

    renderables improvements

--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/renderables/RenderableGuiSprite.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/renderables/RenderableGuiSprite.java
@@ -3,10 +3,11 @@ package io.github.fishstiz.fidgetz.v0.gui.renderables;
 import io.github.fishstiz.fidgetz.v0.utils.GuiGraphicsUtils;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.CommonColors;
 
-record RenderableGuiSprite(Identifier sprite) implements RenderableRectangle {
+record RenderableGuiSprite(Identifier sprite, int color) implements RenderableRectangle {
     @Override
     public void extractRenderState(GuiGraphicsExtractor graphics, int left, int top, int width, int height, int mouseX, int mouseY, float partialTick) {
-        GuiGraphicsUtils.sprite(graphics, sprite, left, top, width, height);
+        GuiGraphicsUtils.sprite(graphics, sprite, left, top, width, height, color);
     }
 }
--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/text/TextComponentUtils.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/text/TextComponentUtils.java
@@ -5,6 +5,7 @@ import net.minecraft.network.chat.Component;
 public final class TextComponentUtils {
     public static final Component BLACK_RIGHT_POINTING_TRIANGLE = Component.literal("▶");
     public static final Component BLACK_DOWN_POINTING_TRIANGLE = Component.literal("▼");
+    public static final Component BLACK_SQUARE = Component.literal("■");
 
     private TextComponentUtils() {
     }
