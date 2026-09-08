5a68443cb25f21f91c36cc3a4309abc486125a60
fishstiz <fishstickz7777@gmail.com>
2026-09-09 01:14:51 +0800

    fix crash in fabric dev

--- testmod/common/src/main/java/io/github/fishstiz/testmod/mixins/MinecraftMixin.java
+++ testmod/common/src/main/java/io/github/fishstiz/testmod/mixins/MinecraftMixin.java
@@ -1,12 +1,17 @@
 package io.github.fishstiz.testmod.mixins;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-    static {
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void audit(GameConfig gameConfig, CallbackInfo ci) {
         MixinEnvironment.getCurrentEnvironment().audit();
     }
 }
