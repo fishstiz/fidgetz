04086831ea8c7790c7b3f6c7554d0715460ae1ca
fishstiz <fishstickz7777@gmail.com>
2026-09-09 03:32:26 +0800

    fix focus related crashes

--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZPopoverMenu.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZPopoverMenu.java
@@ -411,7 +411,7 @@ public class FZPopoverMenu extends FZDialog {
         if (child != null) {
             ComponentPath path = child.menu.nextFocusPath(event);
             if (path != null) {
-                return path;
+                return ComponentPath.path(this, path);
             }
         }
 
