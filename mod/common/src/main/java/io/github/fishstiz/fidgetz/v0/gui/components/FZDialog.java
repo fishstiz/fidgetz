134d854d1f8cce07b10ce2a12096e127d542818c
fishstiz <fishstickz7777@gmail.com>
2026-09-10 03:28:52 +0800

    actually fix dialog focus transfer 2

--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZDialog.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZDialog.java
@@ -96,6 +96,7 @@ public abstract class FZDialog extends FZContainer implements FZComponent, FZPop
     }
 
     protected void onClose() {
+        ComponentPath parentFocusPath = container.getCurrentFocusPath();
         boolean focused = NavigationUtils.inFocusPath(container, this);
 
         ComponentPath path = getCurrentFocusPath();
@@ -104,7 +105,8 @@ public abstract class FZDialog extends FZContainer implements FZComponent, FZPop
         }
 
         if (focused) {
-            container.setFocused(null);
+            NavigationUtils.findParent(parentFocusPath, containerPath -> containerPath.childPath().component() == this)
+                    .ifPresent(parentPath -> parentPath.applyFocus(false));
         }
     }
 
