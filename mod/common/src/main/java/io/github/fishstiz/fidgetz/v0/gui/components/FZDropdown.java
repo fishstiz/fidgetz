d27cb7e0560eb912805c951e35a7b2f61c050fdc
fishstiz <fishstickz7777@gmail.com>
2026-09-10 03:28:52 +0800

    actually fix dialog focus transfer 2

--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZDropdown.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/gui/components/FZDropdown.java
@@ -428,7 +428,7 @@ public final class FZDropdown extends Button.Plain implements FZComponent, FZCon
 
         @Override
         public boolean isFocused() {
-            return isOpen() && (focused || getFocused() != null);
+            return focused || getFocused() != null;
         }
 
         @Override
