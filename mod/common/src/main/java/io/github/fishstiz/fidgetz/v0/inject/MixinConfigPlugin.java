764233021573d7c7a543ba7295e0d72b91c118ea
fishstiz <fishstickz7777@gmail.com>
2026-09-20 23:41:46 +0800

    move debug to mod with optional jvm arg

---
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/inject/MixinConfigPlugin.java
@@ -0,0 +1,47 @@
+package io.github.fishstiz.fidgetz.v0.inject;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        if (System.getProperty("fidgetz.debug") != null) {
+            LoggerFactory.getLogger("fidgetz").info("[fidgetz] Debug enabled");
+            return List.of("debug.GameRendererMixin", "debug.KeyboardHandlerMixin");
+        }
+
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/inject/mixins/debug/KeyboardHandlerMixin.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/inject/mixins/debug/KeyboardHandlerMixin.java
@@ -1,21 +1,22 @@
-package io.github.fishstiz.testmod.mixins;
+package io.github.fishstiz.fidgetz.v0.inject.mixins.debug;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import io.github.fishstiz.testmod.Testmod;
+import io.github.fishstiz.fidgetz.v0.gui.debug.FZDebugOverlay;
 import net.minecraft.client.KeyboardHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings({"unused", "UnusedMixin"})
 @Mixin(KeyboardHandler.class)
 public abstract class KeyboardHandlerMixin {
     @Inject(method = "keyPress", at = @At("HEAD"))
     private void onKeyPress(long handle, int key, int scanCode, int action, int modifiers, CallbackInfo ci) {
         if (action != 0) {
             switch (key) {
-                case InputConstants.KEY_F7 -> Testmod.renderFocusPath = !Testmod.renderFocusPath;
-                case InputConstants.KEY_F8 -> Testmod.renderHovered = !Testmod.renderHovered;
+                case InputConstants.KEY_F7 -> FZDebugOverlay.focusPath = !FZDebugOverlay.focusPath;
+                case InputConstants.KEY_F8 -> FZDebugOverlay.hovered = !FZDebugOverlay.hovered;
             }
         }
     }
