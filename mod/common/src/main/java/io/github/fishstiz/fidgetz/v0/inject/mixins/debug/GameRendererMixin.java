8e47acf21102e897b6a60bb963e3d67966095a56
fishstiz <fishstickz7777@gmail.com>
2026-09-20 23:41:46 +0800

    move debug to mod with optional jvm arg

--- mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/inject/mixins/debug/GameRendererMixin.java
+++ mod/common/src/main/java/io/github/fishstiz/fidgetz/v0/inject/mixins/debug/GameRendererMixin.java
@@ -1,7 +1,7 @@
-package io.github.fishstiz.testmod.mixins;
+package io.github.fishstiz.fidgetz.v0.inject.mixins.debug;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import io.github.fishstiz.testmod.Testmod;
+import io.github.fishstiz.fidgetz.v0.gui.debug.FZDebugOverlay;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.GameRenderer;
@@ -10,8 +10,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings({"unused", "UnusedMixin"})
 @Mixin(GameRenderer.class)
-public abstract class GuiMixin {
+public abstract class GameRendererMixin {
     @Inject(method = "extractGui", at = @At("TAIL"))
     public void extractTestmodRenderState(
             DeltaTracker deltaTracker,
@@ -22,6 +23,6 @@ public abstract class GuiMixin {
             @Local(name = "xMouse") int xMouse,
             @Local(name = "yMouse") int yMouse
     ) {
-        Testmod.extractRenderState(graphics, xMouse, yMouse);
+        FZDebugOverlay.extractRenderState(graphics, xMouse, yMouse);
     }
 }
